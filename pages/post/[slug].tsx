7d65a45ae4d8e420411badc3a9dc3bf101f79bf1
Dhayanandhan <dhayaec@gmail.com>
2021-07-27 18:34:09 +0530

    add dynamic url, active link, analyze

---
+++ pages/post/[slug].tsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+
+const Post = (): JSX.Element => {
+    const router = useRouter()
+    const { slug } = router.query
+    console.error(slug, 'slug');
+
+    return (
+        <>
+            <h1>Slug: {slug}</h1>
+        </>
+    )
+}
+
+export default Post
